e91da31018d6d164b9362406bb8ca540ee06a5e3
SatoMoeko <mopepeko@gmail.com>
2024-01-05 20:30:10 +0900

    追加

--- src/Hiyoco7.java
+++ src/Hiyoco7.java
@@ -2,15 +2,12 @@
 public class Hiyoco7 {
 
 	public static void main(String[]args) {
-		System.out.print("身長を入力してください(cm)>");
-		double height = new java.util.Scanner(System.in).nextDouble();
-		System.out.print("体重を入力してください(kg)>");
-		double weight = new java.util.Scanner(System.in).nextDouble();		
-		System.out.println("あなたのBMIは"+getBMI(height,weight)+"です");
+		System.out.println("お名前を入力してください>");
+		String name = new java.util.Scanner(System.in).nextLine();
+		System.out.println("年齢を入れてください>");
+		int age = new java.util.Scanner(System.in).nextInt();
+		
 	}
-	public static double getBMI(double heightCm, double weightKg) {
-		double getBMI = weightKg/ ((heightCm/ 100)*(heightCm/ 100));
-		return getBMI;
-	}
-
+	public static void greet(String x,int y)
+	
 }
